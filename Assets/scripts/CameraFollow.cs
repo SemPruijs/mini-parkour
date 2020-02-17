aceeacdb5933812bc4610dd2994b9c6303aef858
SemPruijs <sem@pruijs.nl>
2020-02-17 13:14:04 +0100

    Camera follows faster if the distance between the player and the camera is to big

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smoothspeed = 0.125f;
+    public float smoothspeed = 1.3f;
     public Vector3 distanceBetweenObjects;
+    public int fasterDistance = 50;
 
     void FixedUpdate() {
         Vector3 NormalPosition = target.position + distanceBetweenObjects;
         Vector3 FollowPosition = Vector3.Lerp(transform.position, NormalPosition, smoothspeed * Time.deltaTime);
         transform.position = FollowPosition;
+        if (NormalPosition.x - FollowPosition.x >= fasterDistance || NormalPosition.y - FollowPosition.y >= fasterDistance || NormalPosition.x - FollowPosition.x <= -fasterDistance || NormalPosition.y - FollowPosition.y <= -fasterDistance)
+        {
+            smoothspeed = 3f;
+        }
+        else
+        {
+            smoothspeed = 1.3f;
+        }
     }
 
 }
