778f1b29d6f60234e0d35b037f47b142f0f657dc
SemPruijs <sem@pruijs.nl>
2020-02-18 15:17:11 +0100

    Add finish

--- Assets/scripts/LoadScene.cs
+++ Assets/scripts/LoadScene.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            LoadA("Menu");
+        }
+    }
     public void LoadA(string scene)
     {
+        print("hello");
         SceneManager.LoadScene(scene);
     }    
 }
