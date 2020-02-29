6c7e0ac81bcadee0cea085bcad6f7d084268e0ea
SemPruijs <sem@pruijs.nl>
2020-02-29 19:21:48 +0100

    Add level 2

---
+++ Assets/scripts/counter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class counter : MonoBehaviour
+{
+    public static float FinishTime = 0.0f;
+    void Update()
+    {
+        FinishTime += Time.deltaTime;
+        print(FinishTime);
+    }
+}
